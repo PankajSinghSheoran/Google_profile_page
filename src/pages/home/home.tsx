9a48f857bf454095071d405ed7b27605bc511388
PankajSinghSheoran <pankajsheoran728@gmail.com>
2023-02-12 02:46:51 +0530

    Creating Navbar

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,3 +1,37 @@
+import { Form, Input } from "antd";
+import { Header } from "antd/es/layout/layout";
+import React from "react";
+
 export default function Home() {
-  return <div className="bg-red-500">Hello!</div>;
+  return (
+    <div>
+      <Header className="header">
+        <div className="navbar flex-h">
+          <ul>
+            <li>
+              <a href="#">Gmail</a>
+            </li>
+            <li>
+              <a href="#">Image</a>
+            </li>
+            <li>
+              <a href="#">
+                <img
+                  src="https://cdn3.iconfinder.com/data/icons/navigation-and-settings/24/Material_icons-01-11-512.png"
+                  title="Google apps"
+                  width={50}
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </Header>
+      <img width={500} src={require("./googlelogo.png")} alt="googlelogo" />
+      <Form>
+        <Form.Item>
+          <Input placeholder="Search Pankaj Singh Sheoran here" />
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
